f3eb29da2f9c241fa1ef5123ca3e6359bcafc23e
Sertac Ozercan <sozercan@gmail.com>
2024-02-10 07:25:52 +0000

    wip
    Signed-off-by: Sertac Ozercan <sozercan@gmail.com>

--- pkg/aikit2llb/finetune/convert.go
+++ pkg/aikit2llb/finetune/convert.go
@@ -56,7 +56,7 @@ func Aikit2LLB(c *config.FineTuneConfig) (llb.State, *specs.Image) {
 	}
 	state = state.Run(utils.Shf("echo -n \"%s\" > /config.yaml", string(cfg))).Root()
 
-	// state = state.Run(utils.Sh("/provider_unsloth.py")).Root()
+	state = state.Run(utils.Sh("/provider_unsloth.py")).Root()
 
 	// scratch := llb.Scratch().File(llb.Copy(state, "/provider_unsloth.py", "/provider_unsloth.py"))
 
