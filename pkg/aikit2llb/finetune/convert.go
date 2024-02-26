9f272aeb144e67eeb1f5429894d7345345476326
Ubuntu <sozercan@sertac-vm-gpu.trex-ling.ts.net>
2024-02-26 03:37:38 +0000

    update
    Signed-off-by: Ubuntu <sozercan@sertac-vm-gpu.trex-ling.ts.net>

--- pkg/aikit2llb/finetune/convert.go
+++ pkg/aikit2llb/finetune/convert.go
@@ -33,8 +33,8 @@ func Aikit2LLB(c *config.FineTuneConfig) (llb.State, *specs.Image) {
 	}
 
 	// installing dependencies
-	// due to buildkit limitations, we need to install nvidia drivers and driver version must match the host
-	state = state.Run(utils.Sh("apt-get update && apt-get install -y --no-install-recommends python3-dev python3 python3-pip python-is-python3 git wget kmod && cd /root && VERSION=$(cat /proc/driver/nvidia/version | sed -n 's/.*NVIDIA UNIX x86_64 Kernel Module  \\([0-9]\\+\\.[0-9]\\+\\.[0-9]\\+\\).*/\\1/p') && wget https://us.download.nvidia.com/XFree86/Linux-x86_64/$VERSION/NVIDIA-Linux-x86_64-$VERSION.run && chmod +x NVIDIA-Linux-x86_64-$VERSION.run && ./NVIDIA-Linux-x86_64-$VERSION.run -x && rm NVIDIA-Linux-x86_64-$VERSION.run && /root/NVIDIA-Linux-x86_64-$VERSION/nvidia-installer -a -s --skip-depmod --no-dkms --no-nvidia-modprobe --no-questions --no-systemd --no-x-check --no-kernel-modules --no-kernel-module-source && rm -rf /root/NVIDIA-Linux-x86_64-$VERSION"), llb.IgnoreCache).Root()
+	// due to buildkit run limitations, we need to install nvidia drivers and driver version must match the host
+	state = state.Run(utils.Sh("apt-get update && apt-get install -y --no-install-recommends python3-dev python3 python3-pip python-is-python3 git wget kmod && cd /root && VERSION=$(cat /proc/driver/nvidia/version | sed -n 's/.*NVIDIA UNIX x86_64 Kernel Module  \\([0-9]\\+\\.[0-9]\\+\\.[0-9]\\+\\).*/\\1/p') && wget -q https://us.download.nvidia.com/XFree86/Linux-x86_64/$VERSION/NVIDIA-Linux-x86_64-$VERSION.run && chmod +x NVIDIA-Linux-x86_64-$VERSION.run && ./NVIDIA-Linux-x86_64-$VERSION.run -x && rm NVIDIA-Linux-x86_64-$VERSION.run && /root/NVIDIA-Linux-x86_64-$VERSION/nvidia-installer -a -s --skip-depmod --no-dkms --no-nvidia-modprobe --no-questions --no-systemd --no-x-check --no-kernel-modules --no-kernel-module-source && rm -rf /root/NVIDIA-Linux-x86_64-$VERSION"), llb.IgnoreCache).Root()
 
 	// installing cuda
 	cudaKeyringURL := "https://developer.download.nvidia.com/compute/cuda/repos/debian12/x86_64/cuda-keyring_1.1-1_all.deb"
@@ -68,12 +68,15 @@ func Aikit2LLB(c *config.FineTuneConfig) (llb.State, *specs.Image) {
 	state = state.Run(utils.Shf("echo -n \"%s\" > /config.yaml", string(cfg))).Root()
 
 	// setup nvidia devices and run unsloth
-	// due to buildkit limitations, we need to create the devices manually and run unsloth in the same command
+	// due to buildkit run limitations, we need to create the devices manually and run unsloth in the same command
 	state = state.Run(utils.Sh("mknod --mode 666 /dev/nvidiactl c 195 255 && mknod --mode 666 /dev/nvidia-uvm c 235 0 && mknod --mode 666 /dev/nvidia-uvm-tools c 235 1 && mknod --mode 666 /dev/nvidia0 c 195 0 && nvidia-smi && /provider_unsloth.py"), llb.Security(llb.SecurityModeInsecure)).Root()
 
-	// copy the output to scratch which will be the output
-	output := fmt.Sprintf("%s-unsloth.%s.gguf", c.Output.Name, c.Output.Quantize)
-	scratch := llb.Scratch().File(llb.Copy(state, output, output))
+	// copy gguf to scratch which will be the output
+	const inputFile = "*.gguf"
+	copyOpts := []llb.CopyOption{}
+	copyOpts = append(copyOpts, &llb.CopyInfo{AllowWildcard: true})
+	outputFile := fmt.Sprintf("%s-%s.gguf", c.Output.Name, c.Output.Quantize)
+	scratch := llb.Scratch().File(llb.Copy(state, inputFile, outputFile, copyOpts...))
 
 	return scratch, imageCfg
 }
