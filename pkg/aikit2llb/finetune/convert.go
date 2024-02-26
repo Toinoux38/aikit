20784f1487b24d1d8974a935214ce6c65fbfdd6e
Ubuntu <sozercan@sertac-vm-gpu.trex-ling.ts.net>
2024-02-26 00:04:59 +0000

    update
    Signed-off-by: Ubuntu <sozercan@sertac-vm-gpu.trex-ling.ts.net>

--- pkg/aikit2llb/finetune/convert.go
+++ pkg/aikit2llb/finetune/convert.go
@@ -11,10 +11,14 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
+const (
+	// https://github.com/unslothai/unsloth/releases/tag/February-2024
+	unslothCommitSHA = "3e4c5a323c16bbda2c92212b790073c4e99c2a55"
+)
+
 func Aikit2LLB(c *config.FineTuneConfig) (llb.State, *specs.Image) {
 	imageCfg := NewImageConfig(c)
 
-	// TODO: not sure if these are necessary
 	env := map[string]string{
 		"PATH":                       system.DefaultPathEnv("linux") + ":/usr/local/cuda/bin",
 		"NVIDIA_REQUIRE_CUDA":        "cuda>=12.0",
@@ -23,19 +27,16 @@ func Aikit2LLB(c *config.FineTuneConfig) (llb.State, *specs.Image) {
 		"LD_LIBRARY_PATH":            "/usr/local/cuda/lib64",
 	}
 
-	// TODO: need input from the user or set up requirements with nvidia-container-cli
-	state := llb.Image("docker.io/sozercan/nvidia:545-23-06")
+	state := llb.Image(utils.DebianSlim)
 	for k, v := range env {
 		state = state.AddEnv(k, v)
 	}
 
-	// var runOpts []llb.RunOption
-	// runOpts = append(runOpts, llb.Security(llb.SecurityModeInsecure))
+	// installing dependencies
+	// due to buildkit limitations, we need to install nvidia drivers and driver version must match the host
+	state = state.Run(utils.Sh("apt-get update && apt-get install -y --no-install-recommends python3-dev python3 python3-pip python-is-python3 git wget kmod && cd /root && VERSION=$(cat /proc/driver/nvidia/version | sed -n 's/.*NVIDIA UNIX x86_64 Kernel Module  \\([0-9]\\+\\.[0-9]\\+\\.[0-9]\\+\\).*/\\1/p') && wget https://us.download.nvidia.com/XFree86/Linux-x86_64/$VERSION/NVIDIA-Linux-x86_64-$VERSION.run && chmod +x NVIDIA-Linux-x86_64-$VERSION.run && ./NVIDIA-Linux-x86_64-$VERSION.run -x && rm NVIDIA-Linux-x86_64-$VERSION.run && /root/NVIDIA-Linux-x86_64-$VERSION/nvidia-installer -a -s --skip-depmod --no-dkms --no-nvidia-modprobe --no-questions --no-systemd --no-x-check --no-kernel-modules --no-kernel-module-source && rm -rf /root/NVIDIA-Linux-x86_64-$VERSION"), llb.IgnoreCache).Root()
 
-	// state = state.Run(utils.Sh("mknod --mode 666 /dev/nvidiactl c 195 255 && mknod --mode 666 /dev/nvidia-modeset c 195 254 && mknod --mode 666 /dev/nvidia-uvm c 235 0 && mknod --mode 666 /dev/nvidia-uvm-tools c 235 1 && mknod --mode 666 /dev/nvidia0 c 195 0 && chmod 0666 /dev/nvidia* && ls -al /dev && /root/NVIDIA-Linux-x86_64-545.23.06/nvidia-smi"), llb.Security(llb.SecurityModeInsecure)).Root()
-
-	state = state.Run(utils.Sh("apt-get update && apt-get install -y python3 python3-pip python-is-python3 git"), llb.IgnoreCache).Root()
-
+	// installing cuda
 	cudaKeyringURL := "https://developer.download.nvidia.com/compute/cuda/repos/debian12/x86_64/cuda-keyring_1.1-1_all.deb"
 	cudaKeyring := llb.HTTP(cudaKeyringURL)
 	state = state.File(
@@ -43,11 +44,11 @@ func Aikit2LLB(c *config.FineTuneConfig) (llb.State, *specs.Image) {
 		llb.WithCustomName("Copying "+utils.FileNameFromURL(cudaKeyringURL)),
 	)
 	state = state.Run(utils.Sh("dpkg -i cuda-keyring_1.1-1_all.deb && rm cuda-keyring_1.1-1_all.deb")).Root()
-
-	// TODO: use required packages instead of cuda-toolkit if possible
 	state = state.Run(utils.Sh("apt-get update && apt-get install -y --no-install-recommends cuda-toolkit cuda-nvcc-12-3 && apt-get clean")).Root()
-	state = state.Run(utils.Sh("pip install --upgrade pip && pip install packaging torch==2.1.0 ipython")).Root()
-	state = state.Run(utils.Sh("pip install 'unsloth[cu121_ampere] @ git+https://github.com/unslothai/unsloth.git'")).Root()
+
+	// installing unsloth
+	state = state.Run(utils.Sh("pip install --upgrade pip --break-system-packages && pip install packaging torch==2.1.0 ipython --break-system-packages")).Root()
+	state = state.Run(utils.Shf("pip install 'unsloth[cu121_ampere] @ git+https://github.com/unslothai/unsloth.git@%[1]s' --break-system-packages", unslothCommitSHA)).Root()
 
 	// TODO: replace the branch with a release tag and have it update with a release
 	unslothScriptURL := "https://raw.githubusercontent.com/sozercan/aikit/finetune/pkg/finetune/provider_unsloth.py"
@@ -59,15 +60,18 @@ func Aikit2LLB(c *config.FineTuneConfig) (llb.State, *specs.Image) {
 		llb.WithCustomName("Copying "+utils.FileNameFromURL(unslothScriptURL)),
 	)
 
+	// write config to /config.yaml
 	cfg, err := yaml.Marshal(c)
 	if err != nil {
 		panic(err)
 	}
 	state = state.Run(utils.Shf("echo -n \"%s\" > /config.yaml", string(cfg))).Root()
 
-	// TODO: remove ls /dev and nvidia-smi
-	state = state.Run(utils.Sh("mknod --mode 666 /dev/nvidiactl c 195 255 && mknod --mode 666 /dev/nvidia-modeset c 195 254 && mknod --mode 666 /dev/nvidia-uvm c 235 0 && mknod --mode 666 /dev/nvidia-uvm-tools c 235 1 && mknod --mode 666 /dev/nvidia0 c 195 0 && chmod 0666 /dev/nvidia* && ls -al /dev && /root/NVIDIA-Linux-x86_64-545.23.06/nvidia-smi && /provider_unsloth.py"), llb.Security(llb.SecurityModeInsecure)).Root()
+	// setup nvidia devices and run unsloth
+	// due to buildkit limitations, we need to create the devices manually and run unsloth in the same command
+	state = state.Run(utils.Sh("mknod --mode 666 /dev/nvidiactl c 195 255 && mknod --mode 666 /dev/nvidia-uvm c 235 0 && mknod --mode 666 /dev/nvidia-uvm-tools c 235 1 && mknod --mode 666 /dev/nvidia0 c 195 0 && nvidia-smi && /provider_unsloth.py"), llb.Security(llb.SecurityModeInsecure)).Root()
 
+	// copy the output to scratch which will be the output
 	output := fmt.Sprintf("%s-unsloth.%s.gguf", c.Output.Name, c.Output.Quantize)
 	scratch := llb.Scratch().File(llb.Copy(state, output, output))
 
--- website/docs/fine-tune.md
+++ website/docs/fine-tune.md
@@ -4,5 +4,6 @@ title: Fine Tuning
 
 
 ```bash
+docker buildx create --name builder --use --buildkitd-flags '--allow-insecure-entitlement security.insecure'
 docker build --allow security.insecure -f test/aikitfile-unsloth.yaml -output out .
 ```
