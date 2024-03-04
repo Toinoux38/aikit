f882f424d0795382fc5968ba642d27a54ce09837
Sertac Ozercan <sozercan@gmail.com>
2024-03-04 02:07:48 +0000

    update unsloth
    Signed-off-by: Sertac Ozercan <sozercan@gmail.com>

--- pkg/finetune/target_unsloth.py
+++ pkg/finetune/target_unsloth.py
@@ -27,15 +27,16 @@ model, tokenizer = FastLanguageModel.from_pretrained(
 
 model = FastLanguageModel.get_peft_model(
     model,
-    r=16,
-    target_modules=["q_proj", "k_proj", "v_proj", "o_proj",
-                    "gate_proj", "up_proj", "down_proj",],
-    lora_alpha=16,
-    lora_dropout=0,  # Supports any, but = 0 is optimized
-    bias="none",    # Supports any, but = "none" is optimized
-    use_gradient_checkpointing=True,
-    random_state=3407,
-    max_seq_length=max_seq_length,
+    r = 16, # Choose any number > 0 ! Suggested 8, 16, 32, 64, 128
+    target_modules = ["q_proj", "k_proj", "v_proj", "o_proj",
+                      "gate_proj", "up_proj", "down_proj",],
+    lora_alpha = 16,
+    lora_dropout = 0, # Supports any, but = 0 is optimized
+    bias = "none",    # Supports any, but = "none" is optimized
+    use_gradient_checkpointing = True,
+    random_state = 3407,
+    use_rslora = False,  # We support rank stabilized LoRA
+    loftq_config = None, # And LoftQ
 )
 
 # TODO: use the dataset type here
